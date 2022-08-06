1af065f176768ba0a41c1ae0c8e81737f9fef939
DongSub_Joung <ehdtjqwjd@kangwon.ac.kr>
2022-08-06 14:00:56 +0900

    Create 13.3진법_뒤집기.java

---
+++ 13.3진법_뒤집기.java
@@ -0,0 +1,60 @@
+// 자연수 n이 매개변수로 주어집니다. 
+// n을 3진법 상에서 앞뒤로 뒤집은 후, 이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
+
+import java.util.ArrayList;
+
+public class init13 {
+    public static void main(String[] args) {
+        int DATA1= 45, DATA2= 125;
+        initA(DATA1,DATA2);
+        initB(DATA1,DATA2);
+    }
+
+    static void initA(int DATA1, int DATA2){
+        int R1= Sol13.solA(DATA1);
+        int R2= Sol13.solA(DATA2);
+        System.out.println(R1+ "," + R2);
+    }
+    static void initB(int DATA1, int DATA2){
+        int R1= Sol13.solA(DATA1);
+        int R2= Sol13.solA(DATA2);
+        System.out.println(R1+ "," + R2);
+    }
+}
+
+class Sol13 {
+    protected static int solA(int N){
+        String answer= new String();
+
+        while (N>=3){
+//            answer는 문자열 (문자열에 쌓임)
+            answer= answer + (N%3);
+            N/= 3;
+        }
+        answer+= N;
+
+//        Stirng 전체를 하나의 정수로 인식하는 듯
+        return Integer.parseInt(answer, 3);
+    }
+
+    protected static int solB(int N){
+        int answer= 0;
+        ArrayList<Integer> list= new ArrayList<>();
+
+//        10 -> 3
+        while (N != 0){
+            list.add(N%3);
+            N/= 3;
+        }
+
+//        3 -> 10
+        int tmp= 1;
+//        index 끝부터 처음까지
+        for (int i=list.size()-1; i>=0; i--){
+            answer+= list.get(i) * tmp;
+            tmp *= 3;
+        }
+
+        return answer;
+    }
+}
